8d1d25eec13ddb47c3debeaf37a802b7cab03a1c
Collin Cimaroli <collinpcimaroli@lewisu.edu>
2024-11-19 20:43:40 -0600

    Update File.

--- TurtleDraw_cc.py
+++ TurtleDraw_cc.py
@@ -1,8 +1,34 @@
+TEXTFILENAME = 'turtle-draw.txt'
+
 import turtle
 
-print('Turtle Draw Starting...')
+print('')
+print('Turtle Draw - cpc')
+print('')
 
-edTheTurtle = turtle.Turtle()
-edTheTurtle.forward(100)
+cpc = turtle.Turtle()
+cpc.speed(10)
 
+
+print('Reading a text file line by line.')
+print('')
+turtleDrawTextfile = open(TEXTFILENAME, 'r')
+line = turtleDrawTextfile.readline()
+while line:
+    print(line, end='')
+    parts = line.split(' ')
+    
+    if (len(parts) == 3):
+        color = parts[0]
+        x = int(parts[1])
+        y = int(parts[2])
+        
+        cpc.pendown()
+        cpc.goto(x,y)
+    
+    
+    line = turtleDrawTextfile.readline()
+    
 turtle.done()
+print('\nEnd')
+
